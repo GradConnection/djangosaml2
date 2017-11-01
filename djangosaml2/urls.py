e35a18dafa50a341293e9c971a5037219e124230
timogoosenwork <31406553+timogoosenwork@users.noreply.github.com>
2017-11-01 14:52:05 +0200

    changed urls.py for django 1.10 compatibility

--- djangosaml2/urls.py
+++ djangosaml2/urls.py
@@ -26,6 +26,7 @@ urlpatterns = [
     url(r'^acs/$', views.assertion_consumer_service, name='saml2_acs'),
     url(r'^logout/$', views.logout, name='saml2_logout'),
     url(r'^ls/$', views.logout_service, name='saml2_ls'),
+    
     url(r'^ls/post/$', views.logout_service_post, name='saml2_ls_post'),
     url(r'^metadata/$', views.metadata, name='saml2_metadata'),
 ]
